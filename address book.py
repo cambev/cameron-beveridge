bbacb107d95c15eefe79672aee0d0d8ca86642a1
cambev <91449058+cambev@users.noreply.github.com>
2021-09-27 17:46:11 +1000

    Add files via upload

---
+++ address book.py
@@ -0,0 +1,44 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> Author: Cameron Beveridge
+
+>>> # Author: Cameron Beveridge
+>>> # Module: AddressBook.py
+>>> # Description: Contact Information
+
+class person:
+    def __init__(self, first, last, age, phone_number, location, project):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.location = loction
+        self.project = project
+    
+    def full_name(self):
+        print(f'{self.first}{self.last}')
+
+    def_str_(self):
+        return f"{self.first} {self.last} : {self.age} : {self.phone_number} : {self.location} : (self.project}\n"
+        
+>>> print("welcome to the address book")
+welcome to the address book
+>>> print("enter your contact's information")
+enter your contact's information
+>>> 
+>>> first_name = input("first name = ")
+first name = 
+>>> last_name = input("last name = ")
+last name = 
+>>> age = input("age = ")
+Age = 
+>>> phone_number = input("phone number = ")
+phone number = 
+>>> location = input("location = ")
+>>> project = input("project title = ")
+>>> print("Thank you, we have received your contact infromation")
+Thank you, we have received your contact infromation
+>>> our_contact = person(first_name, last name, age, phone_number, location, project)
+>>> out_contact.full_name()
